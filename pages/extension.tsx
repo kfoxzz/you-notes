cf077a5f68f12899a893da53925df915f600b43d
Kristi Zentner <kristi.zentner17@gmail.com>
2023-08-23 12:56:03 -0700

    fix build script, get extension loading in browser

---
+++ pages/extension.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+
+export default function Extension() {
+  // const [minHeight, setMinHeight] = useState<number | undefined>()
+  // const {
+  //   data: keyInsight,
+  //   isLoading,
+  //   mutateAsync,
+  // } = useMutation(findKeyInsight)
+
+  // async function summarizeArticle() {
+  //   // ..
+  // }
+
+  // useEffect(() => {
+  //   summarizeArticle()
+  // }, [])
+
+  return (
+    <div>hellooooooooo</div>
+    // <Layout minHeight={minHeight}>
+    //   <Heading>What is the point?</Heading>
+    //   // ...
+    // </Layout>
+  )
+}
